7de7ee429bd7bce3d8c92aa1dd6f006844a8e985
soffnekos <soffnekos@gmail.com>
2025-11-02 11:26:01 -0500

    Actualizar _level.js

--- plugins/_level.js
+++ plugins/_level.js
@@ -2,12 +2,12 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 
 let handler = m => m
 handler.before = async function (m, { conn }) {
-let user = global.db.data.users[51940631374]
+let user = global.db.data.users[m.sender]
 let before = user.level
 while (canLevelUp(user.level, user.exp, global.multiplier)) { user.level++ }
 if (before !== user.level) {
-let especial = '9999999999'
-let especial2 = '999999999'
+let especial = 'coin'
+let especial2 = 'exp'
 let especialCant = Math.floor(Math.random() * (8000 - 5000 + 1)) + 5000
 let especialCant2 = Math.floor(Math.random() * (500 - 100 + 1)) + 100
 if (user.level % 5 === 0) {
